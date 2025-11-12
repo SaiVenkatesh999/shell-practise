c68a013658fbd21114365be63e25b43adfe41434
SaiVenkatesh999 <saivenkatesh.asv@gmail.com>
2025-11-11 22:39:30 -0600

    shell practise

--- 06-evn-var.sh
+++ 06-evn-var.sh
@@ -2,3 +2,5 @@
 
 echo "Course name is: $COURSE"
 # COURSE is an environment variable that we will set while running this script
+# give export <variable_name>=<value> to set environment variable
+#example: export COURSE="Shell Scripting"
