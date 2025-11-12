7c456563edacc0248a6d97c9cd55f71ec40a7905
SaiVenkatesh999 <saivenkatesh.asv@gmail.com>
2025-11-12 00:01:37 -0600

    shell-practise

--- 10-conditions.sh
+++ 10-conditions.sh
@@ -1,7 +1,20 @@
 NUMBER=$1
-if [ $NUMBER -gt 10 ] ;
-then
-    echo "Number is greater than : 10"
-    else
-    echo "Number is less than or equal to: 10"
+if [ $NUMBER -le 10 ] ; then
+      echo "Number is less than : 10"
+    elif [$NUMBER -eq 10 ] ;   then
+      echo "Number is less than or equal to: 10"
+    else 
+      echo "Number is greater than : 10"
     fi
+
+
+
+#     NUMBER=$1
+
+# if [ $NUMBER -lt 10 ]; then
+#     echo "Given number $NUMBER is less than 10"
+# elif [ $NUMBER -eq 10 ]; then
+#     echo "Given number $NUMBER is equal to 10"
+# else
+#     echo "Given number $NUMBER is greater than to 10"
+# fi
