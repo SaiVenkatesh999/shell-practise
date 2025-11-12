36fe940c2234e4ded431c9e3704b49ffb11f3c48
SaiVenkatesh999 <saivenkatesh.asv@gmail.com>
2025-11-12 00:09:44 -0600

    shell-practise

--- 11-even-odd.sh
+++ 11-even-odd.sh
@@ -2,7 +2,7 @@
 echo "Please enter a number:"
 read -r NUMBER
 if [ $((NUMBER % 2)) -eq 0 ] ; then
-    echo "NUmber is even"
+    echo "given $NUMBER is even"
     else
-    echo "Number is odd"
+    echo "Given $NUMBER is odd"
     fi
