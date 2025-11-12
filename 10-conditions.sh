b4246f579381d2860974a150889a90ab9f898ef2
SaiVenkatesh999 <saivenkatesh.asv@gmail.com>
2025-11-11 23:51:37 -0600

    shell-practise

--- 10-conditions.sh
+++ 10-conditions.sh
@@ -1,7 +1,7 @@
 NUMBER=$1
 if [ $NUMBER -gt 10 ] ;
 then
-    echo "Number is greater than : $NUMBER"
+    echo "Number is greater than : 10"
     else
-    echo "Number is less than or equal to: $NUMBER"
+    echo "Number is less than or equal to: 10"
     fi
